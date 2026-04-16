0e8707edfe76ec5ceb94b448eb972438921ab06b
Roberto Neyviton <rneyviton@gmail.com>
2026-04-16 12:06:00 -0300

    Criação da interface, validação e estilização

--- JavaScript/validacao.js
+++ JavaScript/validacao.js
@@ -1,1 +1,16 @@
 
+// Função para validar se foram inseridos dados antes de enviálos
+
+function validar(){
+
+    let nome = document.getElementById("idnome").value
+    let email = document.getElementById("idemail").value
+
+    if (nome === "" || email === "") {
+        alert("Insira os Dados nos Campos Corretamente!!");
+    }
+    else{
+        alert("Dados corretos.")
+    }
+
+}
--- PHP/index.php
+++ PHP/index.php
@@ -1,1 +1,31 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cadastro de Usuário</title>
+    <link rel="stylesheet" href="../CSS/style.css">
+</head>
+    <body>
+    <h1>Cadastro de Usuário</h1>
 
+        <form action="salvar.php" method="post" autocomplete="on">
+            <div class="login">
+                <p>
+                    <label for="idnome">Nome</label>
+                    <input type="text" name="nome" id="idnome" required minlength="4" maxlength="50" autocomplete="username">
+                </p>
+                    <p>
+                        <label for="idemail">E-mail</label>
+                        <input type="email" name="email" id="idemail" required autocomplete="email">
+                    </p>
+
+                    <button onclick="validar()">Enviar</button>
+                    <input type="reset" value="Limpar">
+            </div>
+
+        </form>
+
+    <script src="../JavaScript/validacao.js"></script>
+    </body>
+</html>
